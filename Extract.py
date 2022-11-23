a8c6648ac9b7d4e6a18dc9d96fea3cd5dc94cc23
Welbert Carvalho <welbertsilveiracarvalho@gmail.com>
2022-11-23 20:50:08 -0300

    Revert "Including a method to export csv files"
    This reverts commit dd96ff0e8918a440ed8226808ae6792dd8c8184e.

--- Extract.py
+++ Extract.py
@@ -55,13 +55,3 @@ class Extract:
         print(df.head())
         return df
         
-    def create_csv_using_df(self, df, target_file_name):
-        path = './exported_dataframes'
-        df.to_csv(
-            f'{path}/{target_file_name}.csv',
-            index = False,
-            sep = ';',
-            encoding = 'utf-8',
-            header = True
-         )
-        return None
