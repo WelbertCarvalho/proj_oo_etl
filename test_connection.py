763600ae6bd10b6a3d9520fa0ffa995344651808
Welbert Carvalho <welbertsilveiracarvalho@gmail.com>
2022-11-23 20:56:12 -0300

    Fixing name folder error

--- test_connection.py
+++ test_connection.py
@@ -9,18 +9,32 @@ instance_extract = Extract()
 # json_data = instance_extract.get_json_data("https://economia.awesomeapi.com.br/json/daily/USD-BRL/0")
 # df = instance_extract.create_df_using_json(json_data)
 
-# df = instance_extract.create_df_using_csv(
-#     csv_path_and_file_name\
-#          = '/home/welbert/Documents/proj_oo/kaggle_datasets/economic_freedom_index2019_data.csv'
-#     )
+economic_freedom = instance_extract.create_df_using_csv(
+    csv_path_and_file_name\
+         = '/home/welbert/Documents/proj_oo/kaggle_datasets/economic_freedom_index2019_data.csv'
+    )
 
 # df = instance_extract.download_kaggle_dataset(
 #     user = "lewisduncan93/the-economic-freedom-index",
 #     file_name = "economic_freedom_index2019_data.csv"
 # )
   
-df_cotacao_moedas = instance_extract\
-    .create_df_reading_sql_file_without_param(
-    'test_proj_oo_ETL', 
-    datalake
+# df_cotacao_moedas = instance_extract\
+#     .create_df_reading_sql_file_without_param(
+#     'test_proj_oo_ETL', 
+#     datalake
+#     )
+
+
+instance_extract\
+    .create_csv_using_df(
+        economic_freedom, 
+        'economic_freedom_test_file'
+        )
+
+
+economic_freedom_2 = instance_extract.create_df_using_csv(
+    csv_path_and_file_name\
+         = '/home/welbert/Documents/proj_oo/exported_dataframes/economic_freedom_test_file.csv',
+         separator = ';'
     )
